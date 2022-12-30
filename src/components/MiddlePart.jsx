9737a50be29a6cb0a93c451e5411a8147b115178
ocsiddisco <celine.lancelle@gmail.com>
2022-12-30 14:56:06 +0100

    add to github

--- src/components/MiddlePart.jsx
+++ src/components/MiddlePart.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import middelpart from "../Images/middelpart.jpg";
 
 function MiddlePart(props) {
     return (
@@ -14,4 +13,3 @@ function MiddlePart(props) {
 
 export default MiddlePart;
 
-// style={{ backgroundImage: `url(${middelpart})`, backgroundRepeat: "no-repeat", width: '100%' , height: '100%', backgroundSize: 'cover' }}
